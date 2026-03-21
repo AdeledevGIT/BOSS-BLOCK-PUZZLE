380ecaaa5f6136e7317ed643c9d8b549874cef97
Adeledev <adeledeveloper7@gmail.com>
2026-03-21 14:10:17 -0400

    k

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'boss-block-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/game.js',
+    '/ui.js',
+    '/firebase-config.js',
+    '/icon.svg',
+    '/background.svg',
+    '/bg1.svg',
+    '/bg2.svg',
+    '/bg3.svg',
+    '/bg4.svg',
+    '/bg5.svg',
+    '/bg6.svg',
+    '/bg7.svg',
+    '/bg8.svg',
+    '/bg9.svg',
+    '/bg10.svg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    // Only intercept same-origin requests to allow Firebase calls to pass unharmed
+    if (e.request.url.startsWith(self.location.origin)) {
+        e.respondWith(
+            caches.match(e.request).then(response => {
+                // Return cached version or fetch from network (stale-while-revalidate pattern or standard fallback)
+                return response || fetch(e.request).catch(() => caches.match('/index.html'));
+            })
+        );
+    }
+});
+
+self.addEventListener('activate', (e) => {
+    // Cleanup old caches if any
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
